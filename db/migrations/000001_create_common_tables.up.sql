17910c9806d8c5c37579d058b25d4250ae0b4811
pendyurinandrey <pendyurinandrey@gmail.com>
2024-10-29 20:09:22 +0300

    Optimized schema

--- db/migrations/000001_create_common_tables.up.sql
+++ db/migrations/000001_create_common_tables.up.sql
@@ -3,13 +3,12 @@ create table users (
     username varchar(64) not null,
     password_hash varchar(128) not null,
     created_at timestamp default now(),
-    updated_at timestamp default now()
+    updated_at timestamp default now(),
+    unique(username)
 );
 
-create unique index idx_unique_username on users(username);
-
 create table accounts (
-    id uuid primary key,
+    id uuid not null,
     user_id uuid not null,
     name varchar(255) not null,
     is_archived bool default false,
@@ -21,39 +20,38 @@ create table accounts (
     region_name varchar(128),
     created_at timestamp default now(),
     updated_at timestamp default now(),
+    primary key (user_id, id),
+    unique(user_id, name),
     constraint fk_accounts_users foreign key(user_id) references users(id)
 );
 
-create unique index idx_unique_account_name on accounts(user_id, name);
-
 create table projects (
-    id uuid primary key,
+    id uuid not null,
     user_id uuid not null,
     name varchar(128) not null,
+    primary key (user_id, id),
+    unique(user_id, name),
     constraint fk_project_users foreign key(user_id) references users(id)
 );
-
-create unique index idx_unique_project_name on projects(user_id, name);
 
 create table fx_rates (
     fx_date date not null,
     currency_code_from varchar(3),
     currency_code_to varchar(3),
-    rate numeric(38, 10)
+    rate numeric(38, 10),
+    unique(fx_date, currency_code_from, currency_code_to)
 );
-
-create unique index idx_unique_fx_rates on fx_rates(fx_date, currency_code_from, currency_code_to);
 
 create table categories (
-    id uuid primary key,
+    id uuid not null,
     user_id uuid not null,
-    name varchar(128)
+    name varchar(128),
+    primary key (user_id, id),
+    unique(user_id, name)  
 );
 
-create unique index idx_unique_categories on categories(user_id, name);
-
 create table operations (
-    id uuid primary key,
+    id uuid not null,
     user_id uuid not null,
     account_id uuid not null,
     project_id uuid not null,
@@ -65,8 +63,9 @@ create table operations (
     operation_type varchar(16) not null,
     created_at timestamp default now(),
     updated_at timestamp default now(),
+    primary key (user_id, id),
     constraint fk_operations_users foreign key(user_id) references users(id),
-    constraint fk_operations_accounts foreign key(account_id) references accounts(id),
-    constraint fk_operations_projects foreign key(project_id) references projects(id),
-    constraint fk_operations_categories foreign key(category_id) references categories(id)
+    constraint fk_operations_accounts foreign key(user_id, account_id) references accounts(user_id, id),
+    constraint fk_operations_projects foreign key(user_id, project_id) references projects(user_id, id),
+    constraint fk_operations_categories foreign key(user_id, category_id) references categories(user_id, id)
 );
